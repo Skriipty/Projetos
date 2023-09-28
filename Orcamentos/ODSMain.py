b68b52f34946ae7b53f30d78992f1b83a265d180
Skriipty <leandro.lucasrato2013@gmail.com>
2023-09-28 14:27:44 -0300

    concerta ae

--- Orcamentos/ODSMain.py
+++ Orcamentos/ODSMain.py
@@ -1,10 +1,12 @@
+from ADD import AdicionarCelular
+
 
 #Dando Entrada no Celular
 nome_cliente = input('Nome do cliente: ')
 
 modelo_celular = input('Modelo do Celular: ')
 
-senha = input('\nSenha Do Cliente: ')
+senha_celular = input('\nSenha Do Cliente: ')
 
 problema = input('\n\nQual o problema do dispositivo?\n\n'
         '(01)Dispositivo Apagado (02)Bateria   (03)Tela\n' 
@@ -16,26 +18,3 @@ problema = input('\n\nQual o problema do dispositivo?\n\n'
         'Escolha uma opção: ')
 
 #laço para caso tiver mais de um problema
-problemas = 0
-prob_adicionais = []
-for problema in range(problemas != 1):
-    
-    tem = int(input('(1)Sim (2)Não\n'
-                'Problemas Adicionais?\n'))
-
-    if tem == 1:
-        problema = input('\n\nQual o problema do dispositivo?\n\n'
-        '(01)Dispositivo Apagado (02)Bateria   (03)Tela\n' 
-        '(04)Camera Trazeira     (05)Placa     (06)Áudio\n' 
-        '(07)Camera Frontal      (08)Microfone (09)Carcaça\n'
-        '(10)Conector de Carga   (11)Botões    (12)Touch\n'
-        '(13)Conector de Fone    (14)Chip      (15)Wi-Fi\n'
-        '(16)Marcas de Mal Uso   (17)Molhado\n\n'
-        'Escolha uma opção: ')
-
-        prob_adicionais.append(problema)
-    
-    else:
-        problemas += 1
-        
-print(prob_adicionais)
