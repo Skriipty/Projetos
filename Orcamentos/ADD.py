e5269552a97166db1f0151b9186562b276099b0d
Skriipty <leandro.lucasrato2013@gmail.com>
2023-09-28 16:20:39 -0300

    Backend Terminado

--- Orcamentos/ADD.py
+++ Orcamentos/ADD.py
@@ -1,13 +1,10 @@
+from Variaveis import dict_problemas
+
 class AdicionarCelular:
-    def __init__(self, nome_cliente, modelo_celular, senha):
-        self.nome_cliente = nome_cliente
-        self.modelo_celular = modelo_celular
-        self.senha = senha
-    
     def select_prob() -> int:
         problema = int(input('\n\nQual o problema do dispositivo?\n\n'
                 '(01)Dispositivo Apagado (02)Bateria   (03)Tela\n' 
-                '(04)Camera Trazeira     (05)Placa     (06)Áudio\n' 
+                '(04)Camera Traseira     (05)Placa     (06)Áudio\n' 
                 '(07)Camera Frontal      (08)Microfone (09)Carcaça\n'
                 '(10)Conector de Carga   (11)Botões    (12)Touch\n'
                 '(13)Conector de Fone    (14)Chip      (15)Wi-Fi\n'
@@ -21,15 +18,28 @@ class AdicionarCelular:
         prob_adicionais = []
         while(problemas != 1):
     
-            tem = int(input('(1)Sim (2)Não\n'
-                'Problemas Adicionais?\n'))
+            tem = int(input('Adicionar Problemas? (1)Sim (2)Não\n:'))
 
             if tem == 1:
 
                 prob_adicionais.append(AdicionarCelular.select_prob())
-    
+
             else:
                 problemas += 1
-                print(prob_adicionais)
 
+                return prob_adicionais
 
+    def procurar_problemas(self, prob_adicionais: list):
+        
+        todos_problemas = []
+        for achado_int in prob_adicionais:
+            
+            if achado_int <= 17:
+                achado_str = dict_problemas[achado_int]
+                todos_problemas.append(achado_str)
+                print('\n', achado_str)
+            
+            else:
+                print('*Opção selecionada não disponivel*')
+        
+        return todos_problemas
--- Orcamentos/ODSMain.py
+++ Orcamentos/ODSMain.py
@@ -1,22 +1,17 @@
 from ADD import AdicionarCelular
-
+from Variaveis import dict_problemas
 
 #Dando Entrada no Celular
-# nome_cliente = input('Nome do cliente: ')
+nome_cliente = input('Nome do cliente: ')
 
-# modelo_celular = input('Modelo do Celular: ')
+modelo_celular = input('\nModelo do Celular: ')
 
-# senha_celular = input('\nSenha Do Cliente: ')
+senha_celular = input('\nSenha Do Cliente: ')
 
-# problema = input('\n\nQual o problema do dispositivo?\n\n'
-#         '(01)Dispositivo Apagado (02)Bateria   (03)Tela\n' 
-#         '(04)Camera Trazeira     (05)Placa     (06)Áudio\n' 
-#         '(07)Camera Frontal      (08)Microfone (09)Carcaça\n'
-#         '(10)Conector de Carga   (11)Botões    (12)Touch\n'
-#         '(13)Conector de Fone    (14)Chip      (15)Wi-Fi\n'
-#         '(16)Marcas de Mal Uso   (17)Molhado\n\n'
-#         'Escolha uma opção: ')
+#laço para adicionar problemas
+cliente_teste = AdicionarCelular()
+prob_adicionais = cliente_teste.adiciona_problemas()
+cliente_teste = cliente_teste.procurar_problemas(prob_adicionais)
 
-#laço para caso tiver mais de um problema
-cliente_teste = AdicionarCelular("getulio vargas", "samsugs 2", "cu")
-cliente_teste.adiciona_problemas()
+#variavel que contem todas as info
+resumo = [nome_cliente, modelo_celular, senha_celular, cliente_teste]
