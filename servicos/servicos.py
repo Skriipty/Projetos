2e5bc99d9816a7e91b31330e02fa85d1e0b0bb13
quites <andersonkpetry@gmail.com>
2023-09-28 17:49:00 -0300

    removendo lixo

--- servicos/servicos.py
+++
@@ -1,9 +0,0 @@
-class Tratamento:
-    def __init__(self, nome_cliente, modelo_celular, senha_do_celular, problemas_adicionais) -> None:
-           self.nome_cliente = nome_cliente
-           self.modelo_celular = modelo_celular
-           self.senha_do_celular = senha_do_celular
-           self.problemas_adicionais = problemas_adicionais
-
-           
-    
