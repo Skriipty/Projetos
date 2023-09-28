000d9e541b582e5cdd6abf329e3ac9c85637bb40
Skriipty <leandro.lucasrato2013@gmail.com>
2023-09-28 13:09:46 -0300

    teste3333

--- ADD.py
+++ ADD.py
@@ -2,3 +2,5 @@ class AdicionarCelular:
     def __init__(self, nome_cliente, modelo_celular, senha):
         self.nome_cliente = nome_cliente
         self.modelo_celular = modelo_celular
+
+#teste
