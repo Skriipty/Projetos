08f93db3856b430698aa9b04cc648cb16a7025a3
quites <andersonkpetry@gmail.com>
2023-09-28 17:29:43 -0300

    fazendo class tratamento

--- servicos/servicos.py
+++ servicos/servicos.py
@@ -1,1 +1,9 @@
-#testando apenas
+class Tratamento:
+    def __init__(self, nome_cliente, modelo_celular, senha_do_celular, problemas_adicionais) -> None:
+           self.nome_cliente = nome_cliente
+           self.modelo_celular = modelo_celular
+           self.senha_do_celular = senha_do_celular
+           self.problemas_adicionais = problemas_adicionais
+
+           
+    
